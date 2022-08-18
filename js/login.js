4e86f6631e18f2fea1fbaf37c48a74c682b76a9d
Md Rasel Khan <immdraselkhan@gmail.com>
2022-08-18 09:39:13 +0600

    first commit

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
-document.getElementById('user-email').addEventListener('click', function(){
-    document.getElementById('user-email').value = 'rasel@baap-er-bank.com';
-})
+// document.getElementById('user-email').addEventListener('click', function(){
+//     document.getElementById('user-email').value = 'rasel@baap-er-bank.com';
+// })
 
-document.getElementById('user-password').addEventListener('click', function(){
-    document.getElementById('user-password').value = 'Password';
-})
+// document.getElementById('user-password').addEventListener('click', function(){
+//     document.getElementById('user-password').value = 'Password';
+// })
 
 document.getElementById('login-btn').addEventListener('click', function(){
     if(document.getElementById('user-email').value === 'rasel@baap-er-bank.com' && document.getElementById('user-password').value === 'Password') {
